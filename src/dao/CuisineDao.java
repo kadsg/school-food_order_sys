bd242feb81703a92baa33900039626295cc98c3f
MingJie Li <kadsg0769@163.com>
2022-11-12 21:16:55 +0800

    实现AllCuisineDao接口，进行功能拓展

--- src/dao/CuisineDao.java
+++ src/dao/CuisineDao.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CuisineDao implements Dao {
